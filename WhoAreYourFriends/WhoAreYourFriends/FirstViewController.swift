2fed6e72d67317bdd8e5e102b8de0990db7fe3f2
edgar wang <edgarwang95@gmail.com>
2016-08-20 12:06:21 -0400

    first ios tabbed project commit

---
+++ WhoAreYourFriends/WhoAreYourFriends/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WhoAreYourFriends
+//
+//  Created by Edgar Wang on 2016-08-20.
+//  Copyright © 2016 Edgar Wang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WhoAreYourFriends/WhoAreYourFriends/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WhoAreYourFriends
+//
+//  Created by Edgar Wang on 2016-08-20.
+//  Copyright © 2016 Edgar Wang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
